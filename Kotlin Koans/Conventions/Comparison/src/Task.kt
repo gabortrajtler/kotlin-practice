7c8ae723ae957a441a2ac775f99c73a60310ee2b
Gabor Trajtler <trajtlerg@gmail.com>
2020-09-08 18:41:31 +0200

    Start implementing "Conventions" koans

--- Kotlin Koans/Conventions/Comparison/src/Task.kt
+++ Kotlin Koans/Conventions/Comparison/src/Task.kt
@@ -1,5 +1,26 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+
+    override fun compareTo(other: MyDate): Int {
+        if (this == other ) return 0
+        if (year > other.year) {
+            return 1
+        } else if (year == year) {
+            if (month > other.month) {
+                return 1
+            } else if (month == other.month) {
+                if (dayOfMonth > other.dayOfMonth) {
+                    return 1
+                }
+            }
+        }
+        return -1
+    }
+
+    fun compareToNicer(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
 }
 
 fun test(date1: MyDate, date2: MyDate) {
