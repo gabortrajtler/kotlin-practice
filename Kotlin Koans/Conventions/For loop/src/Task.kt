a39809404175faf4329ebde0bc7758aee9c03803
Gabor Trajtler <trajtlerg@gmail.com>
2020-09-09 19:01:06 +0200

    Solve some koans in "Conventions"

--- Kotlin Koans/Conventions/For loop/src/Task.kt
+++ Kotlin Koans/Conventions/For loop/src/Task.kt
@@ -1,4 +1,22 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var currentDate = start
+
+            override fun hasNext(): Boolean {
+                return currentDate <= end
+            }
+
+            override fun next(): MyDate {
+                if (!hasNext()) throw NoSuchElementException()
+                val nextDate = currentDate
+                currentDate = currentDate.followingDate()
+                return nextDate
+            }
+        }
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- Kotlin Koans/Conventions/Operators overloading/src/Task.kt
+++ Kotlin Koans/Conventions/Operators overloading/src/Task.kt
@@ -5,12 +5,24 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 // Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return addTimeIntervals(timeInterval, 1)
+}
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval): MyDate {
+    return addTimeIntervals(timeIntervals.timeInterval, timeIntervals.multiplier)
+}
+
+operator fun TimeInterval.times(multiplier: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(this, multiplier)
+}
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val multiplier: Int)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
