eb51a2005b432b4753ad84ec538a0df40fcd39d6
Gabor Trajtler <trajtlerg@gmail.com>
2020-09-20 18:45:58 +0200

    Finish "Collections" koans

--- Kotlin Koans/Collections/Sequences/src/Task.kt
+++ Kotlin Koans/Collections/Sequences/src/Task.kt
@@ -1,14 +1,20 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.orders.asSequence()
+            .filter(Order::isDelivered)
+            .flatMap { it.products.asSequence() }
+            .maxBy(Product::price)
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers.asSequence()
+            .flatMap { it.getOrderedProducts() }
+            .count { it == product }
 }
 
 fun Customer.getOrderedProducts(): Sequence<Product> =
-        TODO()
+        orders.asSequence()
+                .flatMap { it.products.asSequence() }
