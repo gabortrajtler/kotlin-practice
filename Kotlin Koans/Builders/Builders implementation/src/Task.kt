cea0c6962b16949918136597a461439074523689
Gabor Trajtler <trajtlerg@gmail.com>
2020-09-26 21:19:32 +0200

    Solve "Builders" and "Generics" koans

--- Kotlin Koans/Builders/Builders implementation/src/Task.kt
+++ Kotlin Koans/Builders/Builders implementation/src/Task.kt
@@ -13,13 +13,23 @@ fun table(init: TABLE.() -> Unit): TABLE {
 
 class TABLE : Tag("table") {
     fun tr(init: TR.() -> Unit) {
-        /* TODO */
+        val tr = TR()
+        tr.init()
+        children += tr
     }
 }
 
 class TR : Tag("tr") {
     fun td(init: TD.() -> Unit) {
-        /* TODO */
+        val td = TD()
+        td.init()
+        children += td
+    }
+}
+
+class TRnicer : Tag("tr") {
+    fun td(init: TD.() -> Unit) {
+        children += TD().apply(init)
     }
 }
 
--- Kotlin Koans/Generics/Generic functions/src/Task.kt
+++ Kotlin Koans/Generics/Generic functions/src/Task.kt
@@ -1,6 +1,15 @@
 import java.util.*
 
-fun partitionTo() = TODO()
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (element in this) {
+        if (predicate(element)) {
+            first.add(element)
+        } else {
+            second.add(element)
+        }
+    }
+    return Pair(first, second)
+}
 
 fun partitionWordsAndLines() {
     val (words, lines) = listOf("a", "a b", "c", "d e")
