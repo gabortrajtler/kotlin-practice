fd0121105988524ebcb3f35ea333ce46a73acbe9
Gabor Trajtler <trajtlerg@gmail.com>
2020-11-30 08:40:39 +0100

    Refactor refreshTitle() to use coroutines

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,6 +19,9 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -70,6 +73,27 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
                 // If anything throws an exception, inform the caller
                 titleRefreshCallback.onError(
                         TitleRefreshError("Unable to refresh title", cause))
+            }
+        }
+    }
+
+    suspend fun refreshTitle() {
+        // interact with *blocking* network and IO calls from a coroutine
+        withContext(Dispatchers.IO) {
+            val result = try {
+                // Make network request using a blocking call
+                network.fetchNextTitle().execute()
+            } catch (cause: Throwable) {
+                // If the network throws an exception, inform the caller
+                throw TitleRefreshError("Unable to refresh title", cause)
+            }
+
+            if (result.isSuccessful) {
+                // Save it to database
+                titleDao.insertTitle(Title(result.body()!!))
+            } else {
+                // If it's not successful, inform the callback of the error
+                throw TitleRefreshError("Unable to refresh title", null)
             }
         }
     }
